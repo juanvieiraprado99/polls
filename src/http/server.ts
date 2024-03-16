e90ffa8ce4fea35c09ad7e29845373efb480692b
juanvieiraprado99 <juanprado99@gmail.com>
2024-03-16 11:34:40 -0300

    feat: votos no redis e ws para ver votos

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,24 +1,26 @@
 import cookie from "@fastify/cookie";
+import websocket from "@fastify/websocket";
 import fastify from "fastify";
 
 // Rotas
 import { createPoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-poll";
-import { teste } from "./routes/teste";
 import { voteOnPoll } from "./routes/vote-on-poll";
+import { pollResults } from "./ws/poll-results";
 
 const app = fastify();
 
 app.register(cookie, {
   secret: "iuuy87jlkdncklid@iuy9iu3kdjakY!8UIEDNWIUE?",
   hook: "onRequest",
-  parseOptions: {},
 });
+
+app.register(websocket);
 
 app.register(createPoll);
 app.register(getPoll);
 app.register(voteOnPoll);
-app.register(teste);
+app.register(pollResults);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("server runing");
